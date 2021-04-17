2f20660f243d4d090447482efbf81b91e03e9587
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-04-16 19:43:53 -0700

    Automatic deploy of integration at 2021-04-17-02-43-52

---
+++ theme/js/geturl.js
@@ -0,0 +1,25 @@
+function getUrlPath() {
+  var pathname = window.location.pathname;
+  return pathname;
+}
+
+function getBaseUIUrl() {
+  if (baseUIUrl==='') {
+    var protocol = window.location.protocol;
+    var host = window.location.host;
+    return protocol + '//' + host;
+  } else {
+    return baseUIUrl;
+  }
+}
+
+function getBaseApiUrl() {
+  if (baseApiUrl==='') {
+    var protocol = window.location.protocol;
+    var host = window.location.host;
+    return protocol + '//cloud.' + host;
+  } else {
+    return baseApiUrl;
+  }
+}
+
